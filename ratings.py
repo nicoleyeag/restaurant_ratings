70cfa3f8069bdf22eb6b0f7095ef937a6c81280e
Nicole Yeager <nyeager95@gmail.com>
2023-12-04 17:32:39 -0700

    read ratings abc order

--- ratings.py
+++ ratings.py
@@ -1,4 +1,27 @@
 """Restaurant rating lister."""
 
+#create function
+def abc_order_reviews(filename):
+#get each line from file
+    file = open(filename)
 
-# put your code here
+    lines_lst = []
+
+    for lines in file:
+        lines = lines.rstrip().split(":")
+        lines_lst.append(lines)
+
+        print(f"{lines[0]} is rated at {lines[1]}")
+
+
+    # print(lines_lst)
+
+        # for line in lines:
+        #     print(line)
+
+#store in dictionary abc order
+#sorted(lines)
+    
+
+# return new dictionary
+abc_order_reviews("scores.txt")
